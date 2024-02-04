c0b13baa2619b3136d23ef624453e822c5a6d966
krishna jain <115697713+krishna4040@users.noreply.github.com>
2024-02-04 19:28:33 +0530

    fix: fixed await and error response

--- app/api/filters/route.ts
+++ app/api/filters/route.ts
@@ -8,12 +8,16 @@ export const GET = async (req: NextRequest) => {
             `I have a product ${product} which a user wants to buy online, generate all possible filters that a user can put on the product to get his desiered product
             Give response in form of an array of objects so that i can populate the filters on my frontend
             `;
-        const filter = generateAiResponse(prompt);
+        const filter = await generateAiResponse(prompt);
         return NextResponse.json({
             success: true,
+            message: 'data fetched',
             data: filter
         });
-    } catch (error) {
-        console.log(error);
+    } catch (error: any) {
+        return NextResponse.json({
+            success: false,
+            message: error.message
+        })
     }
 }
