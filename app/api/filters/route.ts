e0f362a9280de99b67f1c39ca4d6738720bc9997
D@reK@r@n18 <xpresskaran98@gmail.com>
2024-02-08 22:58:12 +0530

    added the conditionals before and after fetching json

--- app/api/filters/route.ts
+++ app/api/filters/route.ts
@@ -2,22 +2,26 @@ import { NextRequest, NextResponse } from "next/server";
 const { getJson } = require("serpapi");
 
 export const GET = async (req: NextRequest) => {
-    try {
-        const product = req.nextUrl.searchParams.get("product");
-        const json = await getJson({
-            engine: "google_shopping",
-            q: product,
-            api_key: process.env.SERP_API_KEY
-        });
-        return NextResponse.json({
-            success: true,
-            message: 'filters fetch successfully',
-            data: json["filters"]
-        })
-    } catch (error: any) {
-        return NextResponse.json({
-            success: false,
-            message: error.message
-        })
-    }
-}
+  try {
+    const product = req.nextUrl.searchParams.get("product");
+    if (!product) throw new Error("Product not found");
+
+    const json = await getJson({
+      engine: "google_shopping",
+      q: product,
+      api_key: process.env.SERP_API_KEY,
+    });
+    if (!json) throw new Error("Filters not found");
+
+    return NextResponse.json({
+      success: true,
+      message: "filters fetch successfully",
+      data: json["filters"],
+    });
+  } catch (error: any) {
+    return NextResponse.json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
